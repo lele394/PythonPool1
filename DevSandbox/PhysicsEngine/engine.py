856a6117d72419db5bc10df3d4a6ed2f2b6cfc17
“Leo <lele394prod@gmail.com>
2023-09-25 16:34:16 +0200

    leapfrog 1st try, not working

--- DevSandbox/PhysicsEngine/engine.py
+++ DevSandbox/PhysicsEngine/engine.py
@@ -58,3 +58,108 @@ def gravitational_force(r: float, m_bh: float, m: float) -> float:
     return (- ( pc.G * m_bh) / r**2) * m
 
 
+
+
+
+def compute_l0(r, vtheta):
+    """
+    input :
+        r: float
+            r component of the position
+        vtheta :
+            theta component of the speed
+    """
+    return r*r*vtheta
+
+
+
+def theta_next(theta: float, l0: float, rk: float, deltat: float) -> float:
+    """
+    input : float
+        theta : initial theta
+    l0 : float
+        calculated from the initial position
+    rk : float
+        donno
+    deltat :
+        time delta between both steps
+
+
+    UPDATE RK TO THE ALGORITHM IN PDF
+    """
+
+    return theta + l0/(rk**2)* deltat
+
+
+
+
+
+def acceleration(m: float, r: float, l0: float):
+    """
+    input: 
+        m : float
+            mass of black hole
+        r : float
+            radius of the object
+        l0 : float
+            computed angular momentum
+    
+    return :
+        float
+        acceleration
+    """
+    return  - pc.G * m /r**2 + (r-3/2 * Schwarzschild_radius(m) ) * l0**2 / r**4
+
+
+
+
+
+#Leapfrog integration
+
+
+def rk_next(rk: float, vk: float, deltat: float):
+    """
+    input :
+    self explnatory
+
+    return:
+        return rk+1 for leapfrog
+    """
+
+    return rk + vk*deltat
+
+
+def vk_next(vk: float, rk: float, mass: float, l0:float, deltat: float):
+    """
+    input :
+    self explanatory
+    
+    return:
+        return vk+3/2 for leafrog
+    """
+    return vk + acceleration(mass, rk, l0) * deltat
+
+
+def Leapfrog_integrator(object: object, blackhole: object, steps: int, l0: float, deltat: float):
+
+    r = [object.r]
+    v = [object.vr]
+
+    for i in range(steps):
+        r.append( rk_next(r[-1], v[-1], deltat) )
+        v.append( vk_next(v[-1], r[-1], blackhole.m, deltat, l0) )
+
+    return (r, v)
+
+
+
+
+
+
+
+
+
+
+
+
+
--- DevSandbox/PhysicsEngine/test.py
+++ DevSandbox/PhysicsEngine/test.py
@@ -1,4 +1,41 @@
 import engine as e
+import matplotlib.pyplot as plt
+
+
+
+
+
+
+
+steps = 100000
+deltat = 0.01
+
+
+
+
+class object:
+    def __init__(self, r, theta, vr, vtheta, m):
+        self.r = r
+        self.theta= theta
+        self.vr = vr
+        self.vtheta= vtheta
+        self.m = m
+    
+    def speed(self):
+        return (self.vr, self.vtheta)
+
+    def position(self):
+        return (self.r, self.theta)
+    
+
+
+bh = object(0,0,0,0,10**10)
+
+proj = object(5, 0, 0, 0.1, 1000)
+
+
+#for proj
+l0_proj = e.compute_l0(proj.r, proj.vtheta)
 
 
 
@@ -8,6 +45,32 @@ import engine as e
 
 
 
+
+
+
+
+
+
+
+theta = [0]
+r = []
+
+
+#leapfrog returns all r already
+r = e.Leapfrog_integrator(proj, bh, steps, l0_proj, deltat)[0]
+
+#needs to compute all theta using v[]
+for val in r:
+    theta.append( e.theta_next(theta[-1], l0_proj, val, deltat) )
+
+
+
+
+
+
+fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
+ax.plot(theta[10:], r[9:])
+plt.show()
 
 
 
