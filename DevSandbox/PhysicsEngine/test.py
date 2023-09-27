22350caf641913742d361e456086735392e4cbb3
“Leo <lele394prod@gmail.com>
2023-09-28 01:52:20 +0200

    added a poc to fire objects from the position of other objects

--- DevSandbox/PhysicsEngine/test.py
+++ DevSandbox/PhysicsEngine/test.py
@@ -1,6 +1,6 @@
 import engine_rev2 as e
 import matplotlib.pyplot as plt
-from math import sqrt
+from math import sqrt, radians
 
 
 
@@ -18,6 +18,7 @@ projs = [
    # e.object(7, 0, 0, -0.043, 10**4, 0.05),
     e.object(7, 0, 0, vel+0.001, 10**5, 0.05),
     e.object(7, 0, 0, -vel, 10**4, 0.05),
+    e.object(7, 0, 0, 1, 10**4, 0.05),
 ]
 
 
@@ -40,12 +41,25 @@ projs = [
 
 
 
+# & tryna draw a line here
+def DrawVector(object, r, theta, plot, color="blue"):
+    r = [object.r, object.r+r]
+    theta = [object.theta, object.theta + theta]
+    print(r, theta)
+    plot.plot(theta, r, color=color)
+
+
+
+
 # ! for multiple runs
 projectiles = projs
 
 inp = ""
 
-while inp == "":
+#sets darck background
+# plt.style.use('dark_background')
+
+while inp != "q":
 
     projs = list(projectiles)
 
@@ -57,10 +71,14 @@ while inp == "":
     circle = plt.Circle((0, 0), 1, transform=ax.transData._b, color="red", alpha=0.4)
     ax.add_artist(circle)
 
+    
+    
+    tempo_coordinates = []
+    
     for proj in projectiles:
-
         theta = proj.theta_list
         r = proj.r_list
+        tempo_coordinates.append([list(theta), list(r)])
 
 
         ax.plot(theta, r)
@@ -68,9 +86,8 @@ while inp == "":
 
     #reset objects
     projs = list([])
-    to_rm = []
     for p in projectiles:
-        if p.IsOut: to_rm.append(p); pass
+        if p.IsOut: continue
         p.UpdateVariables(deltat)
         projs.append(  e.object(p.r, 
                                 p.theta,
@@ -78,10 +95,7 @@ while inp == "":
                                 p.vtheta,
                                 p.m,
                                 p.radius)    )
-    projectiles = [p for p in projs]
-    for p in to_rm:
-        projectiles.remove(p)
-
+    projectiles = list(projs)
 
 
     if projectiles == []:
@@ -93,6 +107,68 @@ while inp == "":
     plt.draw()
     plt.pause(0.001)
 
-    inp = input("enter to continue > ")
+    inp = input("q to quit > ")
+
+
+
+
+
+
+
+
+
+    # continue
+
+    # * testing implementation of missile firing
+    if inp == "l":
+
+        inp = input("   vr vtheta type\n > ")
+        to_add = []
+        while inp != "c" or inp !="cancel":
+            if inp == "c": break
+
+            inp_split = [float(i) for i in inp.split(" ")]
+
+
+            ship_id = 0
+            ship = projectiles[ship_id]
+
+
+            plt.close()
+            fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
+            circle = plt.Circle((0, 0), 1, transform=ax.transData._b, color="red", alpha=0.4)
+            ax.add_artist(circle)
+
+    
+
+            for proj in tempo_coordinates:
+                theta = proj[0]
+                r = proj[1]
+                ax.plot(theta, r)
+            
+            #draw r and theta vect base
+            DrawVector(ship, 1, 0, ax, "red") #r
+            DrawVector(ship, 0, radians(8), ax, "white") #theta
+
+            #draw launch vector
+            DrawVector(ship, inp_split[0]*100, radians(inp_split[1])*100, ax, "green")
+
+            to_add = [inp_split[0], inp_split[1]]
+
+
+
+
+            plt.draw()
+            plt.pause(0.001)    
+            inp = input("> ")
+        
+
+    if inp == "c":
+        # * adds projectile, mass 10**2 and radius 0.02
+        projectiles.append( e.object(ship.r, ship.theta, to_add[0], to_add[1], 10**2, 0.02)   )
+            
+
+
+
 
 
