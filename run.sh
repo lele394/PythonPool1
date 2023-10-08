b253d258df5d668ee3008480210e0ae62284b9d1
“Leo <lele394prod@gmail.com>
2023-10-08 16:56:51 +0200

    run really works now

--- run.sh
+++ run.sh
@@ -1,15 +1,16 @@
+#!/bin/bash
 cd ressources
 
 # get player input start
 echo "Do you wish to start the game in (r)ender mode or in (s)hell mode?"
 echo "(One letter and case sensitive)"
-read input
+read str1
+echo $str1
 
-if [ $input -eq "r"]
-then 
+if [ $str1 == "r" ]; then 
 python3 render_mode.py
 else
-echo  "This is not the right way to ply the game and does not support any"
+echo  "This is not the right way to play the game and does not support any"
 echo "graphical display. All informations you will get are debug infos"
 echo "from the objects in the simulation. It does show every variables"
 echo "but are in no way meant to be used to play the game."
